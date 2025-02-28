1b0290e83b9d02ac0385fca21f65708a3f20bd15
Yahya Idris Abdurrahman <90093157+Yahya-Idris-A@users.noreply.github.com>
2025-03-01 04:50:54 +0700

    Add feature card component

---
+++ src/components/cards/FeatureCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="flex flex-col items-center bg-white stroke-[#DBDADE] shadow w-fit rounded-[16px]"
+  >
+    <img :src="cover" alt="" class="px-[32px] pt-[32px]" />
+    <h3 class="text-[24px] font-semibold text-[#667085] pt-[15px]">
+      {{ heading }}
+    </h3>
+    <p class="text-[13px] font-semibold text-[#667085] pb-[15px]">
+      {{ description }}
+    </p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  cover: String,
+  heading: String,
+  description: String,
+});
+</script>
--- src/views/landing/Home.vue
+++ src/views/landing/Home.vue
@@ -1,8 +1,15 @@
 <template>
   <BasicNavbar />
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <FeatureCard
+    :cover="FeatureCardImg"
+    heading="Upload"
+    description="Your Existing Video"
+  />
 </template>
 
 <script setup>
 import BasicNavbar from "../../components/navbar/BasicNavbar.vue";
+import FeatureCard from "../../components/cards/FeatureCard.vue";
+import FeatureCardImg from "@assets/img/features/ToddBoehly.png";
 </script>
