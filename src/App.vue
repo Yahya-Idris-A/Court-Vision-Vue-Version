3b6db1e9eeca523526b6b5101f67f48eb520f1ff
Yahya Idris Abdurrahman <90093157+Yahya-Idris-A@users.noreply.github.com>
2025-03-02 03:04:40 +0700

    Add Footer Component And Apply To Landing Page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <BasicNavbar />
   <router-view></router-view>
+  <BasicFooter />
 </template>
 
 <script setup>
 import BasicNavbar from "@components/navbar/BasicNavbar.vue";
+import BasicFooter from "./components/partials/Footer.vue";
 </script>
 
 <style scoped></style>
---
+++ src/components/partials/Footer.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="bg-[#403D91]">
+    <div class="flex flex-col gap-[24px] mx-[70px] py-[24px]">
+      <div class="flex flex-row justify-between">
+        <div class="flex flex-col w-1/3 gap-[24px]">
+          <img
+            src="@assets/img/company Logo/Logo.png"
+            alt=""
+            class="size-[44px]"
+          />
+          <p class="text-white text-[16px] font-normal">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit
+          </p>
+          <div class="flex flex-row gap-[24px]">
+            <v-icon
+              icon="mdi mdi-instagram"
+              class="!text-[24px] text-white"
+            ></v-icon>
+            <v-icon
+              icon="mdi mdi-twitter"
+              class="!text-[24px] text-white"
+            ></v-icon>
+          </div>
+        </div>
+        <div class="flex flex-col w-1/3 gap-[8px]">
+          <h3 class="text-white text-[16px] font-bold">Contact Info</h3>
+          <div class="flex flex-row gap-[8px]">
+            <v-icon
+              icon="mdi mdi-map-marker"
+              class="!text-[24px] text-white"
+            ></v-icon>
+            <p class="text-white text-[14px] font-normal">
+              Ini Alamat Ini Alamat Ini Alamat Ini Alamat Ini Alamat Ini Alamat
+              Ini Alamat Ini Alamat Ini Alamat Ini Alamat Ini Alamat Ini Alamat
+              Ini Alamat Ini Alamat Ini Alamat Ini Alamat Ini Alamat Ini Alamat
+              Ini Alamat Ini Alamat
+            </p>
+          </div>
+          <div class="flex flex-row gap-[8px]">
+            <v-icon
+              icon="mdi mdi-email-outline"
+              class="!text-[24px] text-white"
+            ></v-icon>
+            <div class="flex flex-col gap-[8px]">
+              <p class="text-white text-[14px] font-normal">
+                corporate@bola.com
+              </p>
+              <p class="text-white text-[14px] font-normal">
+                bolabasket@gmail.com
+              </p>
+            </div>
+          </div>
+          <div class="flex flex-row gap-[8px]">
+            <v-icon
+              icon="mmdi mdi-phone"
+              class="!text-[24px] text-white"
+            ></v-icon>
+            <p class="text-white text-[14px] font-normal">+62 82112882656</p>
+          </div>
+        </div>
+      </div>
+      <v-divider horizontal :thickness="3" class="!text-white"></v-divider>
+      <p class="text-white text-[16px] font-bold text-center">
+        © Copyright {{ new Date().getFullYear() }}, HAHAHAHAHAHAHAHAHAHAHAHAH
+      </p>
+    </div>
+  </div>
+</template>
+<script setup></script>
+<style scoped></style>
