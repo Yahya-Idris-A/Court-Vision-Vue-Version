67caea8bbabe93eccdca45f41b2d20d13602a02e
Yahya Idris Abdurrahman <90093157+Yahya-Idris-A@users.noreply.github.com>
2025-03-02 04:36:18 +0700

    Adjust Navbar Text Size And Padding Inline

--- src/components/navbar/BasicNavbar.vue
+++ src/components/navbar/BasicNavbar.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="bg-white sticky top-0 z-50">
     <nav
-      class="flex justify-between items-center px-[80px] py-[18px] bg-white stroke-[#667085] shadow max-sm:px-[30px]"
+      class="flex justify-between items-center px-[100px] py-[18px] bg-white stroke-[#667085] shadow max-sm:px-[30px]"
     >
       <!-- Logo -->
       <div class="">
@@ -13,17 +13,17 @@
       </div>
       <!-- Content -->
       <div
-        class="flex justify-end items-center gap-[24px] max-sm:flex-col max-sm:absolute max-sm:bg-white max-sm:stroke-[#667085] max-sm:shadow max-sm:left-0 max-sm:w-full max-sm:pb-[20px]"
+        class="flex justify-end items-center gap-[64px] max-sm:flex-col max-sm:absolute max-sm:bg-white max-sm:stroke-[#667085] max-sm:shadow max-sm:left-0 max-sm:w-full max-sm:pb-[20px]"
         :class="
           isMenuOpen ? 'max-sm:top-[100%]' : 'max-sm:-top-full max-sm:hidden'
         "
       >
         <!-- Navigation -->
         <div class="">
-          <ul class="flex max-sm:flex-col items-center gap-[24px]">
+          <ul class="flex max-sm:flex-col items-center gap-[64px]">
             <li>
               <a
-                class="hover:text-[#FD6A2A] text-[16px] font-semibold"
+                class="hover:text-[#FD6A2A] text-[20px] font-semibold"
                 :class="
                   $route.name == 'Home' ? 'text-[#FD6A2A]' : 'text-[#667085]'
                 "
@@ -33,7 +33,7 @@
             </li>
             <li>
               <a
-                class="hover:text-[#FD6A2A] text-[16px] font-semibold"
+                class="hover:text-[#FD6A2A] text-[20px] font-semibold"
                 :class="
                   $route.name == 'Features'
                     ? 'text-[#FD6A2A]'
@@ -45,7 +45,7 @@
             </li>
             <li>
               <a
-                class="hover:text-[#FD6A2A] text-[16px] font-semibold"
+                class="hover:text-[#FD6A2A] text-[20px] font-semibold"
                 :class="
                   $route.name == 'Pricing' ? 'text-[#FD6A2A]' : 'text-[#667085]'
                 "
@@ -55,7 +55,7 @@
             </li>
             <li>
               <a
-                class="hover:text-[#FD6A2A] text-[16px] font-semibold"
+                class="hover:text-[#FD6A2A] text-[20px] font-semibold"
                 :class="
                   $route.name == 'Contact' ? 'text-[#FD6A2A]' : 'text-[#667085]'
                 "
@@ -68,12 +68,12 @@
         <!-- login button -->
         <div class="">
           <button
-            class="bg-[#FD6A2A] text-white px-4.5 py-2.5 rounded-[8px] flex gap-[8px] text-[16px] font-semibold items-center cursor-pointer"
+            class="bg-[#FD6A2A] text-white px-4.5 py-2.5 rounded-[8px] flex gap-[8px] text-[20px] font-semibold items-center cursor-pointer"
           >
             Log In
             <img
               src="@assets/img/icon/LogInIcon.png"
-              class="size-[20px]"
+              class="size-[24px]"
               alt=""
             />
           </button>
