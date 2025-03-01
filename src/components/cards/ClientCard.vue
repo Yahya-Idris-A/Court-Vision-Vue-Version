58a28e685ec73a87b8b7bd714d77e2dc5994c22b
Yahya Idris Abdurrahman <90093157+Yahya-Idris-A@users.noreply.github.com>
2025-03-01 07:01:09 +0700

    Add client card component

---
+++ src/components/cards/ClientCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="flex flex-col items-center bg-white stroke-[#DBDADE] shadow w-fit rounded-[16px] max-w-[43.33%]"
+  >
+    <div class="flex flex-row items-center py-[32px] pl-[32px] w-full">
+        <img :src="profileImg" alt="" class="size-[100px]">
+        </img>
+        <div class="flex flex-col pl-[24px]">
+            <h3 class="text-black text-[24px] font-semibold">{{ username }}</h3>
+            <p class="text-[#667085] text-[15px] font-semibold">{{ position }}</p>
+            <div class="flex flex-row items-center">
+                <img :src="teamImg" alt="" class="size-[50px]">
+                <p class="text-[#667085] text-[15px] font-semibold">{{ teamName }}</p>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-row">
+        <v-divider vertical :thickness="2" class="pl-[32px] !text-[#38347A] !my-[5%]"></v-divider>
+        <p class="pl-[10px] pr-[32px] py-[45.34px] text-[13px] font-semibold text-[#667085]">{{ message }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  profileImg: String,
+  username: String,
+  position: String,
+  teamImg: String,
+  teamName: String,
+  message: String,
+});
+</script>
--- src/views/landing/Home.vue
+++ src/views/landing/Home.vue
@@ -1,10 +1,18 @@
 <template>
   <BasicNavbar />
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
   <FeatureCard
     :cover="FeatureCardImg"
     heading="Upload"
     description="Your Existing Video"
+  />
+  <br />
+  <ClientCard
+    :profileImg="profileImg"
+    username="Danny Barth"
+    position="Executive Vice President"
+    :teamImg="teamImg"
+    teamName="Oklahoma City Thunder"
+    message="It is good to analyse the overall movement and shape of the team, for example to identify areas that were not defended during training. The players are asked to check and watch the video every day."
   />
 </template>
 
@@ -12,4 +20,7 @@
 import BasicNavbar from "../../components/navbar/BasicNavbar.vue";
 import FeatureCard from "../../components/cards/FeatureCard.vue";
 import FeatureCardImg from "@assets/img/features/ToddBoehly.png";
+import ClientCard from "@components/cards/ClientCard.vue";
+import profileImg from "@assets/img/clients/client1.png";
+import teamImg from "@assets/img/company Logo/Logo.png";
 </script>
