1873aa21bc262834d3441ffc91c359d91cb08cb9
Yahya Idris Abdurrahman <90093157+Yahya-Idris-A@users.noreply.github.com>
2025-03-01 08:18:30 +0700

    Add pricing card component

---
+++ src/components/cards/PricingCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <div
+    class="flex flex-col items-center bg-white stroke-[#DBDADE] shadow max-w-[26.6%] rounded-[16px]"
+  >
+    <v-icon
+      :icon="pricingIcon"
+      class="!text-[40px] text-[#FD6A2A] !mt-[24px] !mb-[14px]"
+    ></v-icon>
+    <h3 class="text-[24px] text-black font-semibold mb-[14px]">
+      {{ pricingType }}
+    </h3>
+    <h4 class="text-[16px] text-black font-semibold mb-[14px]">{{ cost }}</h4>
+    <h4 class="text-[14px] text-black font-semibold mb-[14px]">
+      {{ billMethod }}
+    </h4>
+    <div class="flex flex-col items-start p-[32px] justify-items-center">
+      <div v-for="(item, index) in benefit" :key="index">
+        <p
+          v-if="item != null"
+          class="text-[#667085] text-[16px] font-normal mb-[16px]"
+        >
+          <v-icon
+            icon="mdi mdi-check-circle-outline"
+            class="!text-[24px] text-[#FD6A2A] !mr-[12px]"
+          ></v-icon
+          >{{ item }}
+        </p>
+      </div>
+    </div>
+    <button
+      class="text-[16px] text-white font-semibold bg-[#FD6A2A] px-[120.5px] py-[12px] rounded-[8px] cursor-pointer mb-[32px]"
+    >
+      Subscribe
+    </button>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  pricingIcon: String,
+  pricingType: String,
+  cost: String,
+  billMethod: String,
+  benefit: Array,
+});
+</script>
--- src/views/landing/Home.vue
+++ src/views/landing/Home.vue
@@ -19,6 +19,14 @@
     question="What Is Basketball?"
     answer="Basketball is a team sport in which two teams, most commonly of five players each, opposing one another on a rectangular court, compete with the primary objective of shooting a basketball through the defender's hoop, while preventing the opposing team from shooting through their own hoop."
   />
+  <br />
+  <PricingCard
+    pricing-icon="mdi mdi-lightning-bolt-circle"
+    pricing-type="Basic"
+    cost="$9 per month"
+    bill-method="Billed every month"
+    :benefit="basicBenefit"
+  />
 </template>
 
 <script setup>
@@ -29,4 +37,12 @@ import ClientCard from "@components/cards/ClientCard.vue";
 import profileImg from "@assets/img/clients/client1.png";
 import teamImg from "@assets/img/company Logo/Logo.png";
 import FAQCard from "../../components/cards/FAQCard.vue";
+import PricingCard from "../../components/cards/PricingCard.vue";
+
+const basicBenefit = [
+  "Access to all basic features",
+  "Basic reporting and analytics",
+  "20GB individual data each user",
+  "Basic chat and email support",
+];
 </script>
